63f553796b27122ffdbc85a6083d5f6883ce297e
Sarthak Agarwal <sarthak.20208110@mnnit.ac.in>
2021-10-09 17:11:14 +0530

    Added button intents and activities.

--- app/src/main/java/com/example/book_a_court/MainActivity.java
+++ app/src/main/java/com/example/book_a_court/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.book_a_court;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button login,reg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login = findViewById(R.id.Login);
+        reg = findViewById(R.id.Register);
     }
+
+    public void login(View view){
+        //intent to open login page
+        Intent intent = new Intent(this, login.class);
+        startActivity(intent);
+    }
+
+    public void register(View view){
+        //intent to open login page
+        Intent intent = new Intent(this, register.class);
+        startActivity(intent);
+    }
+
 }
