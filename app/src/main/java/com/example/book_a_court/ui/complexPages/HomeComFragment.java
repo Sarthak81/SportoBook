43c1e32c719f1d43e999d36a3f216d14ce4e528e
Sarthak Agarwal <sarthak.20208110@mnnit.ac.in>
2021-12-21 17:16:26 +0530

    Complex bookings records created

--- app/src/main/java/com/example/book_a_court/ui/complexPages/HomeComFragment.java
+++ app/src/main/java/com/example/book_a_court/ui/complexPages/HomeComFragment.java
@@ -11,18 +11,58 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.book_a_court.R;
+import com.example.book_a_court.ui.history.HistoryAdapter;
+import com.example.book_a_court.ui.home.BookingData;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class HomeComFragment extends Fragment {
-
+    RecyclerView historyRecycler;
+    HistoryAdapter historyAdapter;
+    FirebaseDatabase db;
+    DatabaseReference personReference;
+    FirebaseAuth fAuth;
+    ArrayList<BookingData> list;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_home_com, container, false);
-        final TextView textView = root.findViewById(R.id.text_homeCom);
+        historyRecycler = root.findViewById(R.id.complex_history_recycler);
 
+        db = FirebaseDatabase.getInstance();
+        fAuth = FirebaseAuth.getInstance();
+        personReference = db.getReference().child("ComplexBookings").child(Objects.requireNonNull(fAuth.getUid()));
+        list = new ArrayList<>();
+
+        personReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                BookingData bookingData = snapshot.getValue(BookingData.class);
+                list.add(bookingData);
+                historyAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        historyRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
+        historyAdapter = new HistoryAdapter(getContext(), list);
+        historyRecycler.setAdapter(historyAdapter);
         return root;
     }
 }
