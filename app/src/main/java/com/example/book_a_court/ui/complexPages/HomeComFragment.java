0175c0620a2b36bc398c207bf2b28bbbb6baa7c7
Sarthak Agarwal <sarthak.20208110@mnnit.ac.in>
2021-12-26 13:39:55 +0530

    UI enhanced

--- app/src/main/java/com/example/book_a_court/ui/complexPages/HomeComFragment.java
+++ app/src/main/java/com/example/book_a_court/ui/complexPages/HomeComFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -50,7 +51,8 @@ public class HomeComFragment extends Fragment {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 BookingData bookingData = snapshot.getValue(BookingData.class);
-                list.add(bookingData);
+                if(bookingData!=null)
+                    list.add(bookingData);
                 historyAdapter.notifyDataSetChanged();
             }
 
